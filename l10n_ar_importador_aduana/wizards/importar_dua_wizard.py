11a82a8602f47c39a0c3ce6c962873b36d6443bb
capirellib <capirellib@gmail.com>
2026-08-12 11:05:20 -0300

    feat: make DUA file field optional and remove confirmation dialog from import wizard

--- l10n_ar_importador_aduana/wizards/importar_dua_wizard.py
+++ l10n_ar_importador_aduana/wizards/importar_dua_wizard.py
@@ -49,6 +49,7 @@ class ImportarDUAWizard(models.TransientModel):
 
     file_data = fields.Binary(
         string='Archivo PDF del DUA',
+        required=False,
         help='Seleccione el archivo PDF del despacho de importación',
     )
     file_name = fields.Char(string='Nombre del Archivo')
